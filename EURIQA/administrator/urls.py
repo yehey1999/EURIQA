24e039f67b09fbc3d4e6c92a84cd0a236ee65627
ajpaulo <alferezjpaul@gmail.com>
2022-02-19 00:02:59 +0800

    Delete unused template

--- EURIQA/administrator/urls.py
+++ EURIQA/administrator/urls.py
@@ -6,7 +6,6 @@ app_name = 'administrator'
 urlpatterns = [
     path('login', views.AdminLoginView.as_view(), name='admin_login'),
     path('logout', views.AdminLogoutView.as_view(), name='admin_logout'),
-    path('home', views.AdminHomeView.as_view(), name='admin_home'),
     path('dashboard', views.AdminDashboard.as_view(), name='admin_dashboard'),
     path('profile', views.AdmminProfile.as_view(), name='admin_profile'),
     path('enrollee-management/regform', views.AdminAccountRegistrationView.as_view(), name='admin_regform'),
